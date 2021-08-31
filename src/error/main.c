b65da8668fe72766f5eec91d41e9a37b2858ac2c
Vadym Khorkov <74003524+Vadrrrr@users.noreply.github.com>
2021-08-31 13:15:37 +0300

    Update main.c

--- src/error/main.c
+++ src/error/main.c
@@ -3,15 +3,4 @@
 int main(int argc, char *argv[]) {
     inv_num(argc);
     error_exist(argc,argv);
-    
-
 }
-
-
-
-// #include <stdio.h>
-// int main() {
-//    // printf() displays the string inside quotation
-//    printf("Hello, World!");
-//    return 0;
-// }
